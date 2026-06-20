62d7a4e82364d2597e59362e182f1cf56332e86d
111202314883 <111202314883@mhs.dinus.ac.id>
2026-06-20 17:51:44 +0700

    fix: ganti interceptor axios pake adapter biar ga error toUpperCase

--- belajar-pintar/src/utils/AxiosInstance.jsx
+++ belajar-pintar/src/utils/AxiosInstance.jsx
@@ -1,7 +1,52 @@
 import axios from "axios";
 import localApi from "./LocalDb";
 
-const isLocal = import.meta.env.VITE_USE_LOCAL_API === "true";
+const isLocal = import.meta.env.VITE_USE_LOCAL_API !== "false";
+
+const localAdapter = async (config) => {
+  const url = new URL(config.url, config.baseURL);
+  const segments = url.pathname.split("/").filter(Boolean);
+  const resource = segments[0];
+  const id = segments[1];
+  const params = { ...Object.fromEntries(url.searchParams.entries()), ...(config.params || {}) };
+
+  if (id) params.id = id;
+
+  let data = config.data;
+  if (typeof data === "string") {
+    try { data = JSON.parse(data); } catch {}
+  }
+
+  let result;
+  switch (config.method) {
+    case "get":
+      result = localApi.get(resource, params);
+      break;
+    case "post":
+      result = localApi.post(resource, data || {});
+      break;
+    case "put":
+      result = localApi.put(resource, id, data || {});
+      break;
+    case "delete":
+      result = localApi.delete(resource, id);
+      break;
+    default:
+      throw new Error("Not found");
+  }
+
+  if (result.status >= 400) {
+    throw new Error("Not found");
+  }
+
+  return {
+    data: result.data,
+    status: result.status,
+    statusText: "OK",
+    headers: result.headers || {},
+    config,
+  };
+};
 
 const AxiosInstance = axios.create({
   baseURL: "http://localhost:3001",
@@ -9,42 +54,9 @@ const AxiosInstance = axios.create({
 });
 
 if (isLocal) {
-  AxiosInstance.interceptors.request.use(async (config) => {
-    const url = new URL(config.url, "http://localhost");
-    const segments = url.pathname.split("/").filter(Boolean);
-    const resource = segments[0];
-    const id = segments[1];
-    const params = { ...Object.fromEntries(url.searchParams.entries()), ...(config.params || {}) };
-
-    if (id) params.id = id;
-
-    let result;
-    switch (config.method) {
-      case "get":
-        result = localApi.get(resource, params);
-        break;
-      case "post":
-        result = localApi.post(resource, config.data || {});
-        break;
-      case "put":
-        result = localApi.put(resource, id, config.data || {});
-        break;
-      case "delete":
-        result = localApi.delete(resource, id);
-        break;
-      default:
-        return config;
-    }
-
-    if (result.status >= 400) {
-      return Promise.reject(new Error("Not found"));
-    }
-
-    return {
-      data: result.data,
-      status: result.status,
-      headers: result.headers || {},
-    };
+  AxiosInstance.interceptors.request.use((config) => {
+    config.adapter = localAdapter;
+    return config;
   });
 }
 
